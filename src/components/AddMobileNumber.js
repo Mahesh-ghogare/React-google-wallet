c526568380a17b03c711988a5b27c8d67e7935bc
Akash Kumar Gaur <149656396+ak45h-twid@users.noreply.github.com>
2024-03-14 12:53:19 +0530

    feat(GOOGLE-WALLET): Changes by Akash

--- src/components/AddMobileNumber.js
+++ src/components/AddMobileNumber.js
@@ -5,57 +5,68 @@ import PoweredBy from './PoweredBy'
 
 function AddMobileNumber() {
 
-    const navigate = useNavigate();
-    function navigateOtp() {
-        navigate('/otp')
-    }
+	const navigate = useNavigate();
+	function navigateOtp() {
+		fetch('https://fakestoreapi.com/products/1', {
+			mobileNumber
+		})
+			.then(res => res.json())
+			.then(json => {
+				console.log(json)
+				navigate('/otp')
+			})
+	}
 
-    const [mobileNumber, setMobileNumber] = useState('');
-    const [isValid, setIsValid] = useState(true);
+	const [mobileNumber, setMobileNumber] = useState('');
+	const [isValid, setIsValid] = useState(true);
 
-    const handleChange = (event) => {
-        const inputValue = event.target.value;
-        setMobileNumber(inputValue);
-        setIsValid(validateMobileNumber(inputValue));
-    };
+	const handleChange = (event) => {
+		const inputValue = event.target.value;
+		setMobileNumber(inputValue);
+		setIsValid(validateMobileNumber(inputValue));
+	};
 
-    const validateMobileNumber = (number) => {
-        const regex = /^[0-9]{10}$/; // Regex to validate 10 digit mobile number
-        return regex.test(number);
-    };
+	const validateMobileNumber = (number) => {
+		const regex = /^[0-9]{10}$/; // Regex to validate 10 digit mobile number
+		return regex.test(number);
+	};
 
-    return (
-        <div>
-            <Header />
-            <div className='verify-mobile-num-sec'>
-                <h3>Verifying Mobile Number</h3>
-                <p>We will send you a verification code to this number</p>
-                <form action="">
-                    <div className='form-outer'>
-                        <div className='contry-code'>
-                            <img src="images/india-flag.svg" alt="flag-img" />
-                            <span>+91</span>
-                        </div>
-                        <div className='form-group'>
-                            <span>Mobile number</span>
-                            {/* <input className='form-control' type="text" /> */}
-                            <input
-                                className='form-control'
-                                type="text"
-                                value={mobileNumber}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        
-                    </div>
-                    {!isValid && <p className='err-msg'>Please enter a valid 10 digit mobile number</p>}
+	return (
+		<div>
+			<Header />
+			<div className='verify-mobile-num-sec'>
+				<h3>Verifying Mobile Number</h3>
+				<p>We will send you a verification code to this number</p>
+				<form action="">
+					<div className='form-outer'>
+						<div className='contry-code'>
+							<img src="images/india-flag.svg" alt="flag-img" />
+							<span>+91</span>
+						</div>
+						<div className='form-group'>
+							<span>Mobile number</span>
+							{/* <input className='form-control' type="text" /> */}
+							<input
+								className='form-control'
+								type="text"
+								value={mobileNumber}
+								onChange={handleChange}
+								maxLength={10}
+							/>
+						</div>
 
-                    <button onClick={navigateOtp} className='comman-btn mt-5'>Send OTP</button>
-                </form>
-            </div>
-            <PoweredBy />
-        </div>
-    )
+					</div>
+					{!isValid && <p className='err-msg'>Please enter a valid 10 digit mobile number</p>}
+
+					<button onClick={(e) => {
+						e.preventDefault();
+						navigateOtp()
+					}} className='comman-btn mt-5'>Send OTP</button>
+				</form>
+			</div>
+			<PoweredBy />
+		</div>
+	)
 }
 
 export default AddMobileNumber
--- src/components/Otp.js
+++ src/components/Otp.js
@@ -1,42 +1,61 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import OtpInput from 'react-otp-input';
 import { useNavigate } from 'react-router-dom'
 import Header from './Header'
 import PoweredBy from './PoweredBy'
 
 const Otp = () => {
+	const [otp, setOtp] = useState('');
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState(false);
 
-    const [otp, setOtp] = useState('');
+	const handleNavigate = useCallback(() => {
+		setLoading(true);
+		fetch('https://fakestoreapi.com/products/1', {
+			otp
+		})
+			.then(res => res.json())
+			.then(json => {
+				console.log(json)
+				navigate('/add-reward', { replace: true });
+			})
+			.catch(() => {
+				setLoading(false)
+			})
+	}, [navigate, otp])
 
-    const navigate = useNavigate();
+	useEffect(() => {
+		if (otp.length === 6) {
+			handleNavigate();
+		}
+	}, [handleNavigate, otp.length])
 
-    function handleNavigate() {
-        navigate('/add-reward', { replace: true })
-    }
+	return (
+		<>
+			<Header />
+			<div className='verify-mobile-num-sec'>
+				<h3>OTP Verifications</h3>
+				<p>Please enter the OTP sent to you via SMS</p>
+				<form action="">
+					<div className='otp-form-outer'>
+						<OtpInput
+							value={otp}
+							onChange={setOtp}
+							numInputs={6}
+							renderSeparator={<span> </span>}
+							renderInput={(props) => <input {...props} />}
+						/>
+					</div>
+					<button disabled={loading} onClick={(e) => {
+						e.preventDefault();
+						handleNavigate()
+					}} className='comman-btn'>{loading ? 'Please wait...' : 'Submit'}</button>
+				</form>
 
-    return (
-        <>
-            <Header />
-            <div className='verify-mobile-num-sec'>
-                <h3>OTP Verifications</h3>
-                <p>Please enter the OTP sent to you via SMS</p>
-                <form action="">
-                    <div className='otp-form-outer'>
-                        <OtpInput
-                            value={otp}
-                            onChange={setOtp}
-                            numInputs={6}
-                            renderSeparator={<span> </span>}
-                            renderInput={(props) => <input {...props} />}
-                        />
-                    </div>
-                    <button onClick={handleNavigate} className='comman-btn'>Submit</button>
-                </form>
-
-                <PoweredBy />
-            </div>
-        </>
-    );
+				<PoweredBy />
+			</div>
+		</>
+	);
 };
 
 export default Otp;
